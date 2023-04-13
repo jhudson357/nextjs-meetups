6fe7dcfc9e95f579ccd0a2b356259a4a3d2cf8a1
James Hudson <jmh357@comcast.net>
2023-04-13 11:43:46 -0500

    add page layout stying

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c1bd947dd9b5e73635c0";
+/******/ 	var hotCurrentHash = "7d4959fcb9f2afbc2f1b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/new-meetup/index.jsx
@@ -0,0 +1,15 @@
+// our-domain.com/new-meetup
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+const NewMeetupPage = () => {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData)
+  }
+  
+  return (
+    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+  )
+}
+
+export default NewMeetupPage
