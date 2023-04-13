f54966450c8cff9538de8b8687326712c567072c
James Hudson <jmh357@comcast.net>
2023-04-13 11:02:47 -0500

    add dummy meetup content to homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ab2569bffff33ab10f61";
+/******/ 	var hotCurrentHash = "c1bd947dd9b5e73635c0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,33 @@
-const Index = () => {
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: '123 City Center, Munich, Germany',
+    description: 'This is the description',
+  },
+  {
+    id: 'm2',
+    title: 'Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: '456 City Center, Munich, Germany',
+    description: 'This is the description',
+  },
+  {
+    id: 'm3',
+    title: 'Third Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: '789 City Center, Munich, Germany',
+    description: 'This is the description',
+  },
+]
+
+const HomePage = () => {
   return (
-    <>
-      <h1>NextJS Meetups</h1>
-    </>
+    <MeetupList meetups={DUMMY_MEETUPS}/>
   )
 }
 
-export default Index
+export default HomePage
